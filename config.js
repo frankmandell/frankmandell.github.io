5fc15796bbbafeb600fc49778808ce8bbd029838
frankmandell <61668615+frankmandell@users.noreply.github.com>
2020-05-06 00:11:28 -0400

    story map

---
+++ config.js
@@ -0,0 +1,116 @@
+var config = {
+    style: 'mapbox://styles/frankmandell/ck9tgi5vj0grs1ipioeq5gv3w',
+    accessToken: 'pk.eyJ1IjoiZnJhbmttYW5kZWxsIiwiYSI6ImNrOXJrdTRsazBwMzgzbGx6OXV1ZmNvNmsifQ.43YEiJiJZUqZJLkPQYAY0w',
+    showMarkers: false,
+    alignment: 'left',
+    theme: 'light',
+    title: 'CASE 01: \n DELTA TO TIST',
+    byline: '',
+    footer: 'karbonfunkc.io',
+    chapters: [
+        {
+            id: 'blackrock_hq',
+            title: "1. BLACKROCK FINANCIAL \n New York, New York",
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/Blackrock%206.jpg',
+            description: 'January 14, 2020, Larry Fink, CEO of BlackRock, an investment Bank headquarter in New York, writes a letter to “CEO”.  The letter is directed towards the CEO’s of all companies insides of BlackRock’s Investment portfolio.  These letters have become famous for shaping global financial and business practices, by using threat of divestment as their catalyst. “Climate change has become a defining factor in companies’ long-term prospects. Last September, when millions of people took to the streets to demand action on climate change, many of them emphasized the significant and lasting impact that it will have on economic growth and prosperity – a risk that markets to date have been slower to reflect. But awareness is rapidly changing, and I believe we are on the edge of a fundamental reshaping of finance.The evidence on climate risk is compelling investors to reassess core assumptions about modern finance. Research from a wide range of organizations – including the UN’s Intergovernmental Panel on Climate Change, the BlackRock Investment Institute, and many others, including new studies from McKinsey on the socioeconomic implications of physical climate risk – is deepening our understanding of how climate risk will impact both our physical world and the global system that finances economic growth.” - Larry Fink',
+            location: {
+                center: [-73.97362, 40.75906],
+                zoom: 16.00,
+                pitch: 51.00,
+                bearing: -17.7
+            },
+        },
+        {
+            id: 'delta_hq',
+            title: '2. DELTA AIRLINES \n Atlanta, Georgia',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/Delta-Air-Lines-Logo.png',
+            description: "Delta Airlines, in response to Fink's letter vows 50% carbon neutrality by 2050, and makes a first investment of $100,000 to purchase carbon offsets for flights to and from Las Vegas, during the 2020 Consumer Electronic Show (now cancelled, due to the COVID-19 pandemic).",
+            location: {
+                center: [-84.41564, 33.64241],
+                zoom: 17.43,
+                pitch: 56.00,
+                bearing: -42.95
+            },
+        },
+       
+        {
+            id: 'growcleanair_hq',
+            title: '3. GROW CLEAN AIR \n Vinalhaven, Maine',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/grow-clean-air-logo_1.png',
+            description: 'Grow Clean Air (GCA), a subsidiary of CAAC, acts as a carbon-clearing house, where individuals or corporations can purchase credits. Their site sells a "tree" for $1USD, and a carbon credit for $12 with little data to substantiate unregulated market prices.  It is not uncommon for corporate actors, like Delta, to advertise carbon neutral conferences or events.',
+            location: {
+                center: [-68.85816, 44.04868],
+                zoom: 19.00,
+                pitch: 55.00,
+                bearing: -136.00
+            },
+        },
+        
+        {
+            id: 'CAAC_hq',
+            title: '4. CLEAN AIR ACTION CORPORATION \n Tulsa, Oklahoma',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/CAAC.png',
+            description: 'Clean Air Action Cooperation is one of the oldest carbon vendors in the US. It was founded in 1993, by Ben and Vanessa Hanneke.',
+            location: {
+                center: [-95.92316, 36.06000],
+                zoom: 16.92,
+                pitch: 51.50,
+                bearing: -26.40
+            },
+        }, 
+        
+        {
+            id: 'tist_Dodoma',
+            title: '5. TIST PLANTING SITE \n Tanzania',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/TIST.png',
+            description: 'From corporate American urban centers, to remote villages in Tanzania, Uganda, Kenya and India, the capital streams narrow to small tributaries, that feed a new growing industry.  The International Small Group Tree Planting Program, better known as TIST is a subsidiary program of CAAC, The Institute for Environmental Innovation (I4EI).',
+            location: {
+                center: [36.672, -6.318],
+                zoom: 15.71,
+                pitch: 32.00,
+                bearing: -24.08
+            },
+        },
+            {
+            id: 'verra_hq',
+            title: '6. VERRA \n Washington D.C.',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/Verra-Logo-Plain-Color.png',
+            description: "After trees have grown to maturity, they are calculated to have captured a certain amount of carbon based on their species, age, and the assumption that they have not been yet deforested. These tree planting projects are then vetted through Verra, an unregulated standards rating agency and verification system. In a financial market laden with corruption and little to no legal oversight, investors rely on independent actors to verify the legitimacy of carbon reduction projects.  Despite intermittant records from TIST, Verra rated TIST's projects in Tanzania soon after their formation. Approval from a CSV like Verra allows new projects to attract international investment.",
+            location: {
+                center: [-77.03094, 38.90630],
+                zoom: 16.62,
+                pitch: 39.50,
+                bearing: 52.80
+            },
+        }, 
+        
+        {
+            id: 'growcleanair_hq',
+            title: '7. Grow Clean Air \n Vinalhaven, Maine',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/grow-clean-air-logo_1.png',
+            description: 'Grow Clean Air, now in possession of VERRA certified Greenhouse Carbon Credits, can begin selling its packaged credits on the open market as “trees”.  One “tree” is a dollar.  Although in the case of Delta and with other larger buys, carbon estimators would decide the size of investment necessary to offset flights or events.',
+            location: {
+                center: [-68.85816, 44.04868],
+                zoom: 18.77,
+                pitch: 55.00,
+                bearing: -136.00
+            },
+        },
+        
+                {
+            id: 'delta_hq',
+            title: '8. Delta Airlines \n Atlanta, Georgia',
+            image: 'https://raw.githubusercontent.com/CenterForSpatialResearch/conflict_urbanism_sp2020/master/images/Karbun_ImagesforStory/Delta-Air-Lines-Logo.png',
+            description: 'Delta, now armed with over $100,000 in carbon offsets, mobilizes its marketing machine and is able to brand themselves as the environmentally friendly airline and a carbon-neutral leader of the world. This greenwashing catches us all. Where we desire to see considerable, verifiable efforts to mitigate carbon emissions, we often stop looking at the announcement of good intentions. Delta refuses to augment the exorbitant fuel consumption of its fleet, and instead, will pour millions into a fraudulant system in order to remain exactly the same.',
+            location: {
+                center: [-84.41564, 33.64241],
+                zoom: 17.43,
+                pitch: 56.00,
+                bearing: -42.95
+            },
+        },
+  
+
+            ]
+};
+
